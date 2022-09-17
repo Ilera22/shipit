dfdf3cc7f22470820507c74bfd87e0e324c02ac3
Ilera <ilera.none@gmail.com>
2022-09-17 00:20:14 +0000

    Update Layout.vue

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -11,7 +11,7 @@
       <v-toolbar-title
         style="width: 350px"
       >
-        <a href="/" class="white--text" style="text-decoration: none"><v-icon>mdi-truck</v-icon>&nbsp;ShipIT</a>
+        <a href="/" class="white--text" style="text-decoration: none">TodoWildrift</a>
       </v-toolbar-title>
       <v-text-field
         flat
